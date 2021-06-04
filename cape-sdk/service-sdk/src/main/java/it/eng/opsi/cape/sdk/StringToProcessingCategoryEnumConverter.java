72806f424acb845c54524e40ada9460ea1326aad
Ermanno Mastrosimone <ermanno.mastrosimone@gmail.com>
2021-06-04 16:15:42 +0200

    Fixed Enum deseralization (Processing e Purpose Category) for QueryParams in Sdk APIs

---
+++ cape-sdk/service-sdk/src/main/java/it/eng/opsi/cape/sdk/StringToProcessingCategoryEnumConverter.java
@@ -0,0 +1,15 @@
+package it.eng.opsi.cape.sdk;
+
+import org.springframework.core.convert.converter.Converter;
+
+import it.eng.opsi.cape.serviceregistry.data.ProcessingCategory;
+
+@RequestParameterConverter
+public class StringToProcessingCategoryEnumConverter implements Converter<String, ProcessingCategory> {
+
+	@Override
+	public ProcessingCategory convert(String source) {
+		return ProcessingCategory.fromValue(source);
+	}
+
+}
