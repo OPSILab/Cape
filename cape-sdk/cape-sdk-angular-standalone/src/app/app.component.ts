01e301cbd68bef39cf724be436709e77d70abf43
Ermanno Mastrosimone <ermanno.mastrosimone@gmail.com>
2021-11-28 19:10:35 +0100

    Minor fixes

--- cape-sdk/cape-sdk-angular-standalone/src/app/app.component.ts
+++ cape-sdk/cape-sdk-angular-standalone/src/app/app.component.ts
@@ -64,6 +64,7 @@ export class AppComponent implements OnInit, OnDestroy {
         state: uuidv4(),
         redirectUri: `${this.appConfig.system.onlineServicesUrl}/login/loginPopup`,
         responseType: NbOAuth2ResponseType.CODE,
+        params: { kc_idp_hint: 'saml' },
       },
       redirect: {
         success: '/pages', // welcome page path
